241da58b35c0a4abed25f3bacee65af8cc5d6d83
irfan <irfancsrrsimt@gmail.com>
2026-02-08 11:40:36 +0530

    loading is added

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,21 +3,41 @@ import { StoreContext } from '../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 import './FoodDisplay.css'
 
-const FoodDisplay = ({category}) => {
-
-    const {food_list} = useContext(StoreContext)
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext)
 
   return (
     <div className='food-display' id='food-display'>
-       <h3>Top Dishes near you</h3>
-       <div className="food-display-list">
-        {food_list&&food_list?.map((item,index)=>{
-          {console.log(category,item.category);}
-          if(category==="All" || category===item.category){
-            return <FoodItem key={index} id={item._id } name={item.name} description={item.description} price={item.price} image={item.image}/>
-          }
-        })}
-       </div>
+      <h3>Top Dishes near you</h3>
+      <div className="food-display-list">
+        {/* Agar food_list empty ya undefined hai to skeleton dikhaye */}
+        {!food_list || food_list.length === 0 ? (
+          <div className="skeleton-wrapper">
+            {[...Array(6)].map((_, i) => (
+              <div key={i} className="skeleton-card">
+                <div className="skeleton-img shimmer"></div>
+                <div className="skeleton-text shimmer"></div>
+                <div className="skeleton-text short shimmer"></div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          food_list.map((item, index) => {
+            if (category === "All" || category === item.category) {
+              return (
+                <FoodItem
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                />
+              )
+            }
+          })
+        )}
+      </div>
     </div>
   )
 }
